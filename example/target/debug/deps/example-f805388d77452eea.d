6736d72f56265fd521c0fbd13cc8e458091b0b3b
Hadi T <hadi@syniol.com>
2025-08-25 20:56:09 +0100

    removed target from example

--- example/target/debug/deps/example-f805388d77452eea.d
+++
@@ -1,5 +0,0 @@
-/Users/hadi/dev/start-ups/xyo/sdks/rust/example/target/debug/deps/libexample-f805388d77452eea.rmeta: src/main.rs
-
-/Users/hadi/dev/start-ups/xyo/sdks/rust/example/target/debug/deps/example-f805388d77452eea.d: src/main.rs
-
-src/main.rs:
