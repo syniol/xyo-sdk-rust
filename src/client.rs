65dd03e443496ab7d013deb52f010d5defc7f4c5
Hadi T <hadi@syniol.com>
2025-08-25 21:37:44 +0100

    draft for client creation

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,13 @@
-struct Client {
+use crate::config::ClientConfig;
+
+pub struct Client {
+    pub config: ClientConfig,
+    http_client: i32,
 }
 
-fn new_client() {
-
+pub fn new_client(config: ClientConfig) -> Client {
+    Client{
+        config,
+        http_client: 0,
+    }
 }
